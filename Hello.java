a9c2db8136a50ce2dd39e682c93bbe4ed37949ce
RaginiKhandelwal <58940718+RaginiKhandelwal@users.noreply.github.com>
2020-03-20 16:43:09 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[]args){
+        System.out.println("hello aabhas");
+        System.out.println("how are you");
+
+        int FirstNumber=2;
+        int SecondNumber=3+2;
+        int ThirdNumber=FirstNumber*2;
+
+        int Total=FirstNumber+SecondNumber+ThirdNumber;
+        int Difference=100-Total;
+        System.out.println("difference=100-"+ Total+"=" + Difference);
+    }
+}
